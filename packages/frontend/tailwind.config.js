267dadb9f16ebf12add6216c19854d8d760f6dcc
i0dheerajdileep <i.dheerajdileep@gmail.com>
2024-08-13 00:18:59 +0530

    feat:face-scan-page-ui

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        pulse: 'pulse 1.5s infinite',
+      },
+    },
   },
   plugins: [],
 }
